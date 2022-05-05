f68809e3ce25edae98ead119d77e555e4daecd59
Vincent <vincent.31@web.de>
2022-05-05 18:45:11 +0200

    TikTakToe V2

--- app/src/main/java/com/example/pma_projekt/MainActivity.java
+++ app/src/main/java/com/example/pma_projekt/MainActivity.java
@@ -11,8 +11,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button b11, b12, b13, b21, b22, b23, b31, b32, b33;
-    String xo = "X";
+    String xo = "O";
     int[][] Storage;
+    boolean noWin = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,45 +47,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button_11:
-                b11.setText(xo);
-                input(1,1);
+                if (input(1,1)){
+                    b11.setText(xo);
+                }
                 break;
             case R.id.button_12:
-                b12.setText(xo);
-                input(1,2);
+                if (input(1,2)){
+                    b12.setText(xo);
+                }
                 break;
             case R.id.button_13:
-                b13.setText(xo);
-                input(1,3);
+                if (input(1,3)){
+                    b13.setText(xo);
+                }
                 break;
             case R.id.button_21:
-                b21.setText(xo);
-                input(2,1);
+                if (input(2,1)){
+                    b21.setText(xo);
+                }
                 break;
             case R.id.button_22:
-                b22.setText(xo);
-                input(2,2);
+                if (input(2,2)){
+                    b22.setText(xo);
+                }
                 break;
             case R.id.button_23:
-                b23.setText(xo);
-                input(2,3);
+                if (input(2,3)){
+                    b23.setText(xo);
+                }
                 break;
             case R.id.button_31:
-                b31.setText(xo);
-                input(3,1);
+                if (input(3,1)){
+                    b31.setText(xo);
+                }
                 break;
             case R.id.button_32:
-                b32.setText(xo);
-                input(3,2);
+                if (input(3,2)){
+                    b32.setText(xo);
+                }
                 break;
             case R.id.button_33:
-                b33.setText(xo);
-                input(3,3);
+                if (input(3,3)){
+                    b33.setText(xo);
+                }
                 break;
+        }
+        if(checkEnd()){
+            gameFinish();
         }
     }
 
-    private void input(int x, int y) {
+
+    private boolean input(int x, int y) {
+        boolean fieldFree = false;
         x = x -1;
         y = y -1;
 
@@ -96,13 +111,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Storage[x][y] = -1;
                 xo = "X";
             }
+            fieldFree = true;
         }
-        if(checkEnd()){
-            gameFinish();
-        }
+        return fieldFree;
     }
 
+
     private boolean checkEnd() {
+        int usedField = 0;
+        for (int x = 0; x<=2; x++){
+            for (int y = 0; y<=2; y++){
+                usedField = usedField + (Math.abs(Storage[x][y]));
+            }
+        }
+
+        if(usedField == 9){
+            noWin = true;
+        }
+
         return    (Math.abs(Storage[0][0] + Storage[0][1] + Storage[0][2]) == 3
                 || Math.abs(Storage[1][0] + Storage[1][1] + Storage[1][2]) == 3
                 || Math.abs(Storage[2][0] + Storage[2][1] + Storage[2][2]) == 3
@@ -110,14 +136,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 || Math.abs(Storage[0][1] + Storage[1][1] + Storage[2][1]) == 3
                 || Math.abs(Storage[0][2] + Storage[1][2] + Storage[2][2]) == 3
                 || Math.abs(Storage[0][0] + Storage[1][1] + Storage[2][2]) == 3
-                || Math.abs(Storage[0][2] + Storage[1][1] + Storage[2][0]) == 3);
+                || Math.abs(Storage[0][2] + Storage[1][1] + Storage[2][0]) == 3)
+                || noWin;
     }
 
+
     private void gameFinish(){
-        if(xo.equals("X")){
-            Toast.makeText(getApplicationContext().getApplicationContext(), "0 gewinnt", Toast.LENGTH_LONG).show();
-        } else {
+        if(noWin){
+            Toast.makeText(getApplicationContext().getApplicationContext(), "Unentschieden", Toast.LENGTH_LONG).show();
+        }
+        else if(xo.equals("X")){
             Toast.makeText(getApplicationContext().getApplicationContext(), "X gewinnt", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext().getApplicationContext(), "O gewinnt", Toast.LENGTH_LONG).show();
         }
         Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);
